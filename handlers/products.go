1d47dbe8920be5dbb696dfd9ea2d6107df12bf0c
Manuel Šarić <manujelko@gmail.com>
2022-10-19 21:20:26 +0200

    Update resouirce and parse url arguments

--- handlers/products.go
+++ handlers/products.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/manujelko/vanilla-go-service/data"
 )
@@ -18,6 +20,17 @@ func NewProducts(l *log.Logger) *Products {
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		p.getProducts(rw, r)
+		return
+	}
+
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
+
+	if r.Method == http.MethodPut {
+		// expect the id in the URI
+		p.updateProduct(rw, r)
 	}
 
 	// catch all
@@ -25,9 +38,51 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 }
 
 func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET Products")
 	lp := data.GetProducts()
 	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Products")
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal error", http.StatusBadRequest)
+	}
+	data.AddProduct(prod)
+}
+
+func (p *Products) updateProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle PUT Products")
+	rex := regexp.MustCompile(`/([0-9]+)`)
+	g := rex.FindAllStringSubmatch(r.URL.Path, -1)
+	if len(g) != 1 || len(g[0]) != 2 {
+		http.Error(rw, "Invalid URI", http.StatusBadRequest)
+		return
+	}
+	idString := g[0][1]
+	id, err := strconv.Atoi(idString)
+	if err != nil {
+		http.Error(rw, "Invalid URI", http.StatusBadRequest)
+		return
+	}
+	prod := &data.Product{}
+	err = prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	err = data.UpdateProduct(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(rw, "Product not found", http.StatusInternalServerError)
+		return
 	}
 }
